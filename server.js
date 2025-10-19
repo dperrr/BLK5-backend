5a5531cc17a19e1bc4b2def2e37b7822ed668f76
dperrr <rosellonjasperdan30@gmail.com>
2025-10-20 02:54:44 +0800

    routing setup

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-require("dotenv").config();
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
+dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
 
+
+connectDB();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.use("/api/auth", authRoutes);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
