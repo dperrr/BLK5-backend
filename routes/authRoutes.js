0a4ae216c308d49412af44bdf90ff32a9bda3f92
dperrr <rosellonjasperdan30@gmail.com>
2025-10-22 03:02:54 +0800

    feat: added an admin route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { register, login } from "../controllers/authController.js";
+import { registerUser, loginUser, getUserProfile, refreshToken} from "../controllers/authController.js";
+import { protect} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/me", protect, getUserProfile, (req, res) => {
+  res.json({ message: "Welcome, authorized user", user: req.user });
+});
+router.get("/refresh", refreshToken);
+
 
 export default router;
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import cookieParser from "cookie-parser";
+
 
 dotenv.config();
 const app = express();
@@ -9,8 +12,10 @@ const app = express();
 
 connectDB();
 
-app.use(express.json());
+app.use(express.json(), cookieParser());
 
 const PORT = process.env.PORT || 5000;
 app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
