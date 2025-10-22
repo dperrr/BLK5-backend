ee70e6fdcb2f4b13dfda0b387532918fd3a53f4f
dperrr <rosellonjasperdan30@gmail.com>
2025-10-23 03:12:22 +0800

    feat: route loader

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import authRoutes from "./authRoutes.js";
+import adminRoutes from "./adminRoutes.js";
+
+const router = express.Router();
+
+
+router.use("/auth", authRoutes);
+router.use("/admin", adminRoutes);
+
+
+export default router;
