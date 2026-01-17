9f6ec93b90448581d33a347e4f6083e0c83df280
dperrr <rosellonjasperdan30@gmail.com>
2026-01-18 03:50:50 +0800

    feat: add CORS middleware for API requests

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors"; 
 import routes from "./routes/index.js";  
 import listEndpoints from "express-list-endpoints";
 dotenv.config();
 const app = express();
 
 connectDB();
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 app.use(cookieParser());
