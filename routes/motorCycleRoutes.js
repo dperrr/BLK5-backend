57e56a3802524e34851b4c9d6676c8fed5c50b21
dperrr <rosellonjasperdan30@gmail.com>
2026-01-24 03:46:05 +0800

    update: updated the routes for motorcycle

--- routes/motorCycleRoutes.js
+++ routes/motorCycleRoutes.js
@@ -1,14 +1,15 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
 import { createMotorcycle, getMotorcycles, getMotorcycleById, updateMotorcycle, deleteMotorcycle, } from "../controllers/motorcycleController.js";
+import { upload } from '../config/cloudinary.js';
 
 
 const router = express.Router();
 
-router.post("/", protect, createMotorcycle);
-router.get("/", protect, getMotorcycles);
+router.post("/", protect, upload.single("image"), createMotorcycle);
+router.get("/motors", protect, getMotorcycles);
 router.get("/:id", protect, getMotorcycleById);
-router.put("/:id", protect, updateMotorcycle);
+router.put("/:id", protect, upload.single("image"), updateMotorcycle);
 router.delete("/:id", protect, deleteMotorcycle);
 
 export default router;
